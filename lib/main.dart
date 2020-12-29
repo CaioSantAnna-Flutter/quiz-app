6bdfea4f9eccd0fa4d0a7af6da27fcef27a0120b
CaioSantAnna <caio.santanna@sintesesolucoes.com.br>
2020-12-29 15:56:39 -0300

    Continuação...

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 //StatelessWidget só aceita renderizar widgets com informações alteradas externamente
 //StatefulWidget aceita atualizar a renderização de widgets com atualização de informação interna também
@@ -24,10 +25,31 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color ?',
-      'What\'s your favorite animal ?'
+    const questions = [
+      {
+        'questionText': 'What\'s your favorite color ?',
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': 'What\'s your favorite animal ?',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      },
+      {
+        'questionText': 'What\'s your favorite season ?',
+        'answers': ['Summer', 'Spring', 'Autumn', 'Winter']
+      },
     ];
+
+    // 3 formas diferentes de vincular uma ação a um evento qualquer do widget, nesse caso, do click do botão
+    //onPressed: _answerQuestion
+    //onPressed: () => print('Answer 2 choosen!')),
+    //onPressed: () { ('Answer 3 choosen!'); })
+
+    // Versão do children do body antes de usar o map para renderizar as opções possíveis de resposta para cada pergunta
+    //Question(questions[_questionIndex]['questionText']),
+    //Answer(_answerQuestion),
+    //Answer(_answerQuestion),
+    //Answer(_answerQuestion),
 
     return MaterialApp(
       home: Scaffold(
@@ -36,16 +58,15 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            RaisedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
-            RaisedButton(
-                child: Text('Answer 2'),
-                onPressed: () => print('Answer 2 choosen!')),
-            RaisedButton(
-                child: Text('Answer 3'),
-                onPressed: () {
-                  print('Answer 3 choosen!');
-                }),
+            Question(
+              questions[_questionIndex]['questionText'],
+            ),
+            //Spread operator -- pega uma lista de elementos e retira eles dela, adicionando cada um dos elementos individualmente. Ao invés de
+            // retornar uma lista a uma lista, ele retorna os valores dessa lista, de forma que o resultado não seja uma lista encadeada
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList(),
           ],
         ),
       ),
